c1ea440a6f2a5ab02deef046c7061c6ddad1e05b
Bilal Ashfaque <bilalashfaque@Bilals-MacBook-Air.local>
2026-05-23 04:13:53 +0530

    Support single connection URI (DATABASE_URL) and default parameters for MySQL in db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,15 +2,24 @@ import { Sequelize } from "sequelize";
 import dotenv from "dotenv";
 dotenv.config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
+let sequelize;
+
+if (process.env.DATABASE_URL) {
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: "mysql",
     logging: console.log, // show SQL queries
-  }
-);
+  });
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME || "formbuilder",
+    process.env.DB_USER || "root",
+    process.env.DB_PASSWORD || "Mbappe@10",
+    {
+      host: process.env.DB_HOST || "localhost",
+      dialect: "mysql",
+      logging: console.log, // show SQL queries
+    }
+  );
+}
 
 export default sequelize;
